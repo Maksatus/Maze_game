a78e787c224d86a994a12f3a9deea02b71350e54
Max <streetworkout042000@gmail.com>
2020-10-26 19:19:42 +0400

    Добавил пак джостиков и управление

--- Assets/Script/move_.cs
+++ Assets/Script/move_.cs
@@ -1,46 +1,55 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 public class move_ : MonoBehaviour
 {
     public float Speed = 10f;
     private Rigidbody _rb;
-
-
-
+    public Quaternion calibrationQuaternion;
+    public Joystick joystick;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        
+        CalibtationAccelerometr();
+        joystick = FindObjectOfType<Joystick>(); 
     }
 
-    // обратите внимание что все действия с физикой 
+    // обратите внимание что все действия с физикой
     // необходимо обрабатывать в FixedUpdate, а не в Update
     void FixedUpdate()
     {
-        MovementLogic();;
+        MovementLogic(); ;
+    }
+
+    public void CalibtationAccelerometr()
+    {
+        Vector3 accelerometrSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerometrSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+    public Vector3 FixeAcceleration(Vector3 accelerometr)
+    {
+        Vector3 fixedAccelerometr = calibrationQuaternion * accelerometr;
+        return fixedAccelerometr;
     }
 
     private void MovementLogic()
     {
-        Vector3 movement = new Vector3();
         //пк
         /*float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);*/
 
         // android
-        Vector3 acceleration = Input.acceleration;
-        if (acceleration.sqrMagnitude > 1)
-            acceleration.Normalize();
+        //Vector3 accelerationRaw = Input.acceleration;
+        //Vector3 acceleration = FixeAcceleration(accelerationRaw);
+        //if (acceleration.sqrMagnitude > 1)
+        //    acceleration.Normalize();
+        //  Vector3 movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
 
-         movement = new Vector3(acceleration.x, 0.0f, acceleration.y);
+        Vector3 movement = new Vector3(joystick.Horizontal, 0.0f, joystick.Vertical);
         _rb.AddForce(movement * Speed);
-
-
     }
 
 }
