9c59ceb592d680d10d1e15d0cf3579d07dd1d101
Max <streetworkout042000@gmail.com>
2020-10-29 02:44:55 +0400

    Signed-off-by: Max <streetworkout042000@gmail.com> Добавил немного красоты

--- Assets/Script/barrier_script/MoveThorns.cs
+++ Assets/Script/barrier_script/MoveThorns.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveThorns : MonoBehaviour
@@ -17,7 +16,6 @@ public class MoveThorns : MonoBehaviour
         while (true)
         {
             yield return StartCoroutine(Utils.MoveToTarget(EnemyPrefab.transform, new Vector3(EnemyPrefab.transform.position.x, EnemyPrefab.transform.position.y+dist, EnemyPrefab.transform.position.z)));
-
             yield return StartCoroutine(Utils.MoveToTarget(EnemyPrefab.transform, new Vector3(EnemyPrefab.transform.position.x, EnemyPrefab.transform.position.y - dist, EnemyPrefab.transform.position.z)));
             yield return new WaitForSeconds(2);
         }
