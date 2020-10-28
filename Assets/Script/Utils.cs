4c20333c91c60b3251d64fa7d119e425e4fa66d0
Max <streetworkout042000@gmail.com>
2020-10-29 01:42:01 +0400

    Signed-off-by: Max <streetworkout042000@gmail.com> Добавил колья и скрипт для их передвижения.

---
+++ Assets/Script/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils : MonoBehaviour
+{
+    public static IEnumerator MoveToTarget(Transform obj, Vector3 target)
+    {
+        Vector3 startPosition = obj.position;
+        float t = 0;
+
+        const float animationDuration = 1.5f;
+        while (t<1)
+        {
+            obj.position = Vector3.Lerp(startPosition, target, t * t*t);
+            t += Time.deltaTime / animationDuration;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Script/barrier_script/MoveThorns.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveThorns : MonoBehaviour
+{
+    public GameObject EnemyPrefab;
+    public float dist = 2;
+
+    private void Start()
+    {
+        StartCoroutine(CoroutSample());
+    }
+
+    private IEnumerator CoroutSample()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(Utils.MoveToTarget(EnemyPrefab.transform, new Vector3(EnemyPrefab.transform.position.x, EnemyPrefab.transform.position.y+dist, EnemyPrefab.transform.position.z)));
+
+            yield return StartCoroutine(Utils.MoveToTarget(EnemyPrefab.transform, new Vector3(EnemyPrefab.transform.position.x, EnemyPrefab.transform.position.y - dist, EnemyPrefab.transform.position.z)));
+            yield return new WaitForSeconds(2);
+        }
+
+    }
+}
