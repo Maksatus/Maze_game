c3ffdbb6069ceb14df2c055aedbcbef958bafee7
Max <streetworkout042000@gmail.com>
2020-10-31 17:10:18 +0400

    Загрузочный экран добавил

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -1,25 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void NewGame()
     {
-        SceneManager.LoadScene(1);
+        SceneTransition.SwitchToScene("Game Scene");
     }
     public void Exit()
     {
