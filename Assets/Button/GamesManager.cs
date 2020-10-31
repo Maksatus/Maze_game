2a0bf5df6cbf9f40f247871ca1a5fdb7399eb90c
Max <streetworkout042000@gmail.com>
2020-11-01 01:18:02 +0400

    Добавил паузу

---
+++ Assets/Button/GamesManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GamesManager : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    bool status = false;
+    public GameObject pauseMenuUI;
+    public GameObject JoystickActiv;
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        JoystickActiv.SetActive(true);
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneTransition.SwitchToScene("Start");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    void Pause_menu()
+    {
+        JoystickActiv.SetActive(false);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void Pause()
+    {
+        Pause_menu();
+    }
+
+}
