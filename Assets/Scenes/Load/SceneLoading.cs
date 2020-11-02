aa73ce5ab825c2a9ede0a2449b562dcfc6277735
Max <streetworkout042000@gmail.com>
2020-11-02 18:30:34 +0400

    добавил загрузку

---
+++ Assets/Scenes/Load/SceneLoading.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour
+{
+    [Header("Загружаемая сцена:")]
+    public int sceneID;
+    [Space]
+    public Image LoadingImg;
+    public Text progressText;
+    public GameObject panel;
+
+    void Start()
+    {
+        StartCoroutine(AsyncLoad());
+        panel.SetActive(false);
+
+    }
+    IEnumerator AsyncLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            LoadingImg.fillAmount = progress;
+            progressText.text = string.Format("{0:0}%", progress * 100);
+            yield return null;
+        }
+    }
+}
