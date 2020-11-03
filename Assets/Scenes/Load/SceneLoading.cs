f4372d7bfac8ef41e51a9e578ae2d2efd9bbdfe3
Max <streetworkout042000@gmail.com>
2020-11-03 07:25:13 +0400

    Кривая загрузка выполнена

--- Assets/Scenes/Load/SceneLoading.cs
+++ Assets/Scenes/Load/SceneLoading.cs
@@ -11,22 +11,39 @@ public class SceneLoading : MonoBehaviour
     public Image LoadingImg;
     public Text progressText;
     public GameObject panel;
+    public GameObject loadingScren;
+    public Text tapText;
 
-    void Start()
+    public void Load()
     {
-        StartCoroutine(AsyncLoad());
+        loadingScren.SetActive(true);
         panel.SetActive(false);
-
+        StartCoroutine(AsyncLoad());
     }
+
     IEnumerator AsyncLoad()
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
-        while (!operation.isDone)
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneID);
+        asyncLoad.allowSceneActivation = false;
+        while (!asyncLoad.isDone)
         {
-            float progress = operation.progress / 0.9f;
+            float progress = asyncLoad.progress / 0.9f;
             LoadingImg.fillAmount = progress;
             progressText.text = string.Format("{0:0}%", progress * 100);
+            if (asyncLoad.progress>=0.9f && !asyncLoad.allowSceneActivation)
+            {
+                StartCoroutine(Text());
+                if(Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+                }
+            }
             yield return null;
         }
+    }
+    IEnumerator Text()
+    {
+        yield return new WaitForSeconds(2);
+        tapText.text = string.Format("Tap to screen!");
     }
 }
