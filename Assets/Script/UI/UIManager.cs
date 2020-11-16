2835205e077a6d05ffd35345f8e8e92ee43f0e7a
Max <streetworkout042000@gmail.com>
2020-11-16 13:06:17 +0400

    up

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -3,7 +3,8 @@ using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    public RectTransform mainMenu, levelMenu,settingMenu;
+    public RectTransform mainMenu, levelMenu, shopMenu;
+   
     private void Start()
     {
         mainMenu.DOAnchorPos(Vector2.zero,0.25f);
@@ -14,21 +15,32 @@ public class UIManager : MonoBehaviour
         levelMenu.DOAnchorPos(new Vector2(0,0), 0.25f);
     }
 
-    public void ControlsJoyskick()
-    {
-        move_.MoveControls = false;
-    }
-    public void ControlsAccelerometer()
-    {
-        move_.MoveControls = true;
-    }
-
     public void closeLevel()
     {
         mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
         levelMenu.DOAnchorPos(new Vector2(2070, 0), 0.25f);
     }
+    public void Shop()
+    {
+        mainMenu.DOAnchorPos(new Vector2(-2070, 0), 0.25f);
+        shopMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
 
+    public void closeShop()
+    {
+        mainMenu.DOAnchorPos(new Vector2(0, 0), 0.25f);
+        shopMenu.DOAnchorPos(new Vector2(2070, 0), 0.25f);
+    }
+
+    public void ControlsJoyskick()
+    {
+        move_.MoveControls = false;
+    }
+    
+    public void ControlsAccelerometer()
+    {
+        move_.MoveControls = true;
+    }
     public void Exit()
     {
         Application.Quit();
