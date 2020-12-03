b9e0855815ae8d2f205f3d2b5845ce7e222bebd3
Max <streetworkout042000@gmail.com>
2020-12-04 01:21:43 +0400

    огненный шар

---
+++ Assets/Script/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]private float speed = 5f;
+    [SerializeField]private float lifeTime = 10f;
+    protected void Start()
+    {
+        Destroy(gameObject,lifeTime);
+    }
+
+    protected void Update()
+    {
+        transform.Translate(0,0,speed * Time.deltaTime);
+    }
+}
