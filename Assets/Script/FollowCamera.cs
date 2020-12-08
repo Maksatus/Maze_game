4c79713de82b28ba622d98c8142e1f090c96ec11
Max <streetworkout042000@gmail.com>
2020-12-08 16:20:24 +0400

    Замена главного персонажа

--- Assets/Script/FollowCamera.cs
+++ Assets/Script/FollowCamera.cs
@@ -2,11 +2,13 @@
 
 public class FollowCamera : MonoBehaviour
 {
-    public Transform target;
-    public float smooth = 5.0f;
-    public Vector3 offset = new Vector3(0, 2, -5);
+    //follow player
+    
+    [SerializeField] private Transform target;
+    [SerializeField] private float smooth = 5.0f;
+    [SerializeField] private Vector3 offset = new Vector3(0, 0, 0);
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * smooth);
+        transform.position = Vector3.MoveTowards(transform.position, target.position + offset, Time.deltaTime * smooth);
     }
 }
